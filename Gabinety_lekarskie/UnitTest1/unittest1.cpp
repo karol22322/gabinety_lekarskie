0562392c5f26f2312cfd16148a7b220c2e2e1fa1
Krzysztof <56804867+kmarcinek2k@users.noreply.github.com>
2020-01-26 23:46:15 +0100

    Tests
    Pare testow

--- Gabinety_lekarskie/UnitTest1/unittest1.cpp
+++ Gabinety_lekarskie/UnitTest1/unittest1.cpp
@@ -18,6 +18,31 @@ namespace UnitTest1
 			Assert::AreEqual(t.Czy_wolny(), ter);
 
 		}
+		TEST_METHOD(TestMethod2)
+		{
+			Termin t;
+			t.Pesel("99803212");
+			string pesel = "99803212";
+			Assert::AreEqual(t.Print_Pesel(), pesel);
+
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			Termin t(1,2,"W");
+			t.Rezerwuj();
+			bool ter = false;
+			Assert::AreEqual(t.Czy_wolny(), false);
+
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			Termin t(1, 2, "W");
+			t.Pesel("99803212");
+			string pesel = "99803212";
+			bool ter = true;
+			Assert::AreEqual(t.Czy_Pesel(pesel), true);
+
+		}
 
 	};
 }
